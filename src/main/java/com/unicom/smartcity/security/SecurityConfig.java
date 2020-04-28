0bc53d6dc081b7c0c7cc1a51c26dfe6711473336
liukai <zhizhufan@foxmail.com>
2020-04-28 17:05:05 +0800

    init

---
+++ src/main/java/com/unicom/smartcity/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.unicom.smartcity.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author liukai
+ */
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .oauth2Login();
+//                .loginPage("/login/oauth2");
+    }
+
+}
